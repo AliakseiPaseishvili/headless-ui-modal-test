21f0d04a06723398daced6556d8c460c318ddfce
Aliaksei Paseishvili <aliaksei.paseishvili@gmail.com>
2026-01-09 18:15:49 +0400

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,6 @@
-"use client";
-import { ModalV1 } from "./components/ModalV1";
-import { TestContent } from "./components/TestContent";
-import { useState } from "react";
-import { Button } from "./components/Button";
-import { Modal } from "./components/Modal";
+import React from "react";
+import { Test } from "@/components/Test";
 
 export default function Home() {
-  const [isOpenV1, setIsOpenV1] = useState(false);
-  const [isOpenV2, setIsOpenV2] = useState(false);
-
-  return (
-    <>
-      <Button onClick={() => setIsOpenV1((v) => !v)}>ToggleModal1</Button>
-      <Button onClick={() => setIsOpenV2((v) => !v)}>ToggleModal2</Button>
-      <ModalV1 isOpened={isOpenV1} onClose={() => setIsOpenV1(false)}>
-        <TestContent onClose={() => setIsOpenV1(false)} />
-      </ModalV1>
-      <Modal show={isOpenV2} onClose={() => setIsOpenV2(false)}>
-        <TestContent onClose={() => setIsOpenV2(false)} />
-      </Modal>
-    </>
-  );
+  return <Test />;
 }
---
+++ src/components/Test.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React, { useState } from 'react';
+import { ModalV1 } from "../components/ModalV1";
+import { TestContent } from "../components/TestContent";
+import { Button } from "../components/Button";
+import { Modal } from "../components/Modal";
+
+export const Test = () => {
+  const [isOpenV1, setIsOpenV1] = useState(false);
+  const [isOpenV2, setIsOpenV2] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsOpenV1((v) => !v)}>ToggleModal1</Button>
+      <Button onClick={() => setIsOpenV2((v) => !v)}>ToggleModal2</Button>
+      <ModalV1 isOpened={isOpenV1} onClose={() => setIsOpenV1(false)}>
+        <TestContent onClose={() => setIsOpenV1(false)} />
+      </ModalV1>
+      <Modal show={isOpenV2} onClose={() => setIsOpenV2(false)}>
+        <TestContent onClose={() => setIsOpenV2(false)} />
+      </Modal>
+    </>
+  );
+}
