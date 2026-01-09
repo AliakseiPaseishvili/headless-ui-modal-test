8c7a25aa50e82d87c3879377bab5cbd1d9685823
Aliaksei Paseishvili <aliaksei.paseishvili@gmail.com>
2026-01-09 20:17:36 +0400

    added div

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -10,7 +10,7 @@ export const Test = () => {
   const [isOpenV2, setIsOpenV2] = useState(false);
 
   return (
-    <>
+    <div>
       <Button onClick={() => setIsOpenV1((v) => !v)}>ToggleModal1</Button>
       <Button onClick={() => setIsOpenV2((v) => !v)}>ToggleModal2</Button>
       <ModalV1 isOpened={isOpenV1} onClose={() => setIsOpenV1(false)}>
@@ -19,6 +19,6 @@ export const Test = () => {
       <Modal show={isOpenV2} onClose={() => setIsOpenV2(false)}>
         <TestContent onClose={() => setIsOpenV2(false)} />
       </Modal>
-    </>
+    </div>
   );
 }
