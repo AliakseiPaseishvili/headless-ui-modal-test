681ba5958ac61571cc3eb9ac5d739db7fc1bb945
Aliaksei Paseishvili <aliaksei.paseishvili@gmail.com>
2026-01-09 20:27:01 +0400

    added viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { Test } from "@/components/Test";
+import { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+};
 
 export default function Home() {
   return <Test />;
