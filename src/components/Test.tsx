f415117648e1efd0832b03449b3270c0d418b025
Aliaksei Paseishvili <aliaksei.paseishvili@gmail.com>
2026-01-09 20:34:10 +0400

    added inline overflow hidden

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -10,7 +10,7 @@ export const Test = () => {
   const [isOpenV2, setIsOpenV2] = useState(false);
 
   return (
-    <div className='fixed'>
+    <div className='fixed' style={{ overflow: 'hidden', }}>
       <Button onClick={() => setIsOpenV1((v) => !v)}>ToggleModal1</Button>
       <Button onClick={() => setIsOpenV2((v) => !v)}>ToggleModal2</Button>
       <ModalV1 isOpened={isOpenV1} onClose={() => setIsOpenV1(false)}>
