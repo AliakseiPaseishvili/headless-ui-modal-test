f3d3b6eae8a3c5c0851383a67d604e8bf6b60627
Aliaksei Paseishvili <aliaksei.paseishvili@gmail.com>
2026-01-09 21:14:28 +0400

    added propably a fix

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -97,7 +97,7 @@ export const Modal: FC<ModalProps> = ({
             >
               <DialogPanel
                 className={twMerge(
-                  'md:h-auto transform overflow-visible rounded-md bg-white text-left align-middle shadow-sb transition-all border border-gray-200',
+                  'md:h-auto transform overflow-hidden rounded-md bg-white text-left align-middle shadow-sb transition-all border border-gray-200',
                   MODAL_SIZE[type],
                   isFullScreen ? 'w-full h-full' : 'h-auto',
                   !isMobileMenu && 'py-6',
--- src/components/ModalV1/ModalV1.tsx
+++ src/components/ModalV1/ModalV1.tsx
@@ -19,7 +19,7 @@ export const ModalV1: FC<PropsWithChildren<ModalProps>> = ({
     <Dialog onClose={onClose}>
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
-          <DialogPanel className=" inline-block transform overflow-hidden rounded-lg text-left align-bottom shadow-xl transition-all sm:my-8 w-1/2 sm:align-middle">
+          <DialogPanel className="inline-block transform overflow-hidden rounded-lg text-left align-bottom shadow-xl transition-all sm:my-8 w-1/2 sm:align-middle">
             {children}
           </DialogPanel>
         </div>
